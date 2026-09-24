65fc0195c2fe19cd26a3590c77ccc89a308d818f
Oracle Public Cloud User <opc@giftique-erpnext.subnet09241103.vcn09241103.oraclevcn.com>
2026-09-24 14:19:24 +0000

    Load PROXC branding on web login

--- proxc/hooks.py
+++ proxc/hooks.py
@@ -27,6 +27,8 @@ app_license = "mit"
 # include js, css files in header of desk.html
 app_include_css = "/assets/proxc/css/proxc.css"
 app_include_js = "/assets/proxc/js/proxc.js"
+web_include_css = "/assets/proxc/css/proxc.css"
+web_include_js = "/assets/proxc/js/proxc.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/proxc/css/proxc.css"
